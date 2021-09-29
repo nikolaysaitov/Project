529b06c68bfa3424b163c91be8d3fac9d9961e91
Nikolay <saitovkms@gmail.com>
2021-09-29 11:28:33 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const category='toys';
+
+console.log(`https://www.udemy.com/${category}/5`);
+
+const user='Ivan';
+
+alert(`privet ${user}`);
+
+let incr=10,
+    decr=10;
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
